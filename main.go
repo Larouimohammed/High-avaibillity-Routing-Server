d401f74f37fbb6baa965d1ff49cb2c721b76be55
Larouimohammed <m.laroui@esi-sba.dz>
2023-09-13 17:35:34 +0000

    latest

--- main.go
+++ main.go
@@ -10,17 +10,19 @@ func main() {
 	wg := &sync.WaitGroup{}
 	P := routerplugin.NewRouter("R1", "primary", true, []byte{192, 168, 1, 12}, []byte{255, 255, 255, 0}, "enp")
 	S := routerplugin.NewRouter("R2", "Secondary", false, []byte{192, 168, 1, 13}, []byte{255, 255, 255, 0}, "enp")
-	wg.Add(2)
+	wg.Add(3)
 	go P.Run(wg)
 	go S.Watcher(P, wg)
-	for ev := range db.Messages {
-		if ev == "change router" {
-			wg.Add(2)
-			go S.Run(wg)
-			go P.Watcher(S, wg)
+	go func(wg *sync.WaitGroup) {
+		for ev := range db.Messages {
+			if ev == "change router" {
+				go S.Run(wg)
+				go P.Watcher(S, wg)
 
+			}
+			defer wg.Done()
+			defer close(db.Messages)
 		}
-		defer close(db.Messages)
-	}
+	}(wg)
 	wg.Wait()
 }
